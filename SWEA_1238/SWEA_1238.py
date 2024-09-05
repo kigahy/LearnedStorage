09c3b8e9eacefeb76b70a9ef78dc3881a8e496df
kimgahyeonn <in47821@gmail.com>
2024-09-05 11:43:01 +0900

    0905 11:42

---
+++ SWEA_1238/SWEA_1238.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def DFS(start) :
+
+for tc in range(10) :
+
+    length, start = map(int, input().split()) # 정점 수, 시작점
+    lst = list(map(int, input().split())) # from, to, from, to
+    dict = {}
+
+    for i in range(len(lst)//2) :
+        v1 = lst[i*2] # lst의 i번째 값을 v1 변수에 할당
+        v2 = lst[i*2+1] # lst의 i+1번째 값을 v2 변수에 할당
+
+        dict[v1] = v2 # from : to, 단방향
+
+    result = DFS(dict)
+
